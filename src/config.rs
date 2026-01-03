cf5f4cdaad530df85cd622e2cc3b408f8508871f
haozi <haozi596@163.com>
2026-01-03 21:55:58 +0800

    111111

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.113.144.246"];
+pub const RS_PUB_KEY: &str = "UHaVWKi0MvR7LEqJ02245Tp79gH4JGwODXkHLE6+0RE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
